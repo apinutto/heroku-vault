ce061e3c43b4b70ff9f8696d5f2bf9063faf0860
GpMidi <paul@gpmidi.net>
2021-04-13 16:30:07 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -11,18 +11,17 @@ cat > /tmp/config.json << EOF
 ui = ${ENABLE_UI:=false}
 disable_mlock = true
 
-cluster_name = "${HEROKU_APP_NAME:=vault}"
-
+#cluster_name = "${HEROKU_APP_NAME:=vault}"
 #api_addr = "https://$HEROKU_DYNO_ID:$PORT"
 #cluster_addr = "http://$HEROKU_DYNO_ID:8080"
 
 storage "postgresql" {
   connection_url = "${DATABASE_URL:?}"
-  ha_enabled = true
+  #ha_enabled = true
 }
 listener "tcp" {
  address = "0.0.0.0:$PORT"
- cluster_address = "0.0.0.0:8080"
+ #cluster_address = "0.0.0.0:8080"
  tls_disable = 1
 }
 EOF
